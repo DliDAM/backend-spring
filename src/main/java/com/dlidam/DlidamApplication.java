941a9e278478f53587fbf667028173f841e28080
Yoon-Jemin <yjm1800@naver.com>
2024-08-01 13:58:28 +0900

    first commit

---
+++ src/main/java/com/dlidam/DlidamApplication.java
@@ -0,0 +1,13 @@
+package com.dlidam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlidamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlidamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlidam/DlidamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlidam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlidamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
