f29288ab063781c469dc38b7869d5927bd57217c
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-08-23 23:32:44 +0900

    socket.io Hostname을 server-ip로 설정

--- src/main/java/com/dlidam/configuration/webrtc/SocketIOConfiguration.java
+++ src/main/java/com/dlidam/configuration/webrtc/SocketIOConfiguration.java
@@ -11,7 +11,7 @@ public class SocketIOConfiguration {
     @Bean
     public SocketIOServer socketIOServer(ConfigUtil configUtil) {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        config.setHostname("0.0.0.0");
+        config.setHostname(configUtil.getSocketServer());
         config.setPort(Integer.parseInt(configUtil.getSocketPort()));
         config.setTransports(Transport.WEBSOCKET, Transport.POLLING);
 
