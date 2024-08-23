0479eaca8b69845e5fcb5e95c2d8c083bd800599
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-08-23 22:07:00 +0900

    cors설정 추가

--- src/main/java/com/dlidam/configuration/CorsConfig.java
+++ src/main/java/com/dlidam/configuration/CorsConfig.java
@@ -9,21 +9,31 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 
 @Configuration
-public class CorsConfig  {
+public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://localhost3000",
-                "https://localhost:8000","http://3.34.121.34", "http://43.203.3.70" , "ws://43.203.3.70"));
 
+        // CORS 설정에 ws://, wss:// 프로토콜 허용
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080", "https://localhost:3000",
+                "https://localhost:8000", "http://3.34.121.34", "http://15.164.220.167",
+                "ws://15.164.220.167", "wss://15.164.220.167", "ws://3.34.121.34", "wss//3.34.121.34"));
+
+        // 모든 헤더 허용
         config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type","Cookie","Set-Cookie"));
 
+        // 허용된 HTTP 메서드
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // 노출할 헤더
+        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type", "Cookie", "Set-Cookie"));
+
+        // 모든 경로에 대해 위의 설정 적용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
